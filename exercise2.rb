36b74a22011a4c64c8c4a5d7efa69900045b7ffb
Eric Marrone <emarrone17@gmail.com>
2017-11-08 13:12:05 -0500

    Add exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,9 +1,13 @@
-names = ["Joe", "Bob", "Sally"]
 
 def in_list?(name)
-  
-
+  names = ["Joe", "Bob", "Sally"]
+  if names.include?(name)
+    puts "Greetings #{name}!"
+  else
+    puts "Who goes there?"
+  end
 end
 
-
+puts "Enter your name..."
 user_name = gets.chomp
+in_list?(user_name)
