486f58bdfeaa9b994d6ab209bf131a2aac752255
Eric Marrone <emarrone17@gmail.com>
2017-11-08 13:07:22 -0500

    Add alternate method for exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,9 +1,21 @@
+# def sum_odd(numbers)
+#   odd = []
+#   numbers.each do |number|
+#     if number % 2 > 0
+#       odd << number
+#     else
+#     end
+#   end
+#   return odd.sum
+# end
+#
+# puts sum_odd([1, 2, 3, 4, 5])
+
 def sum_odd(numbers)
   odd = []
-  numbers.each do |number|
-    if number % 2 > 0
+  numbers.select do |number|
+    if number.odd?
       odd << number
-    else
     end
   end
   return odd.sum
---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+names = ["Joe", "Bob", "Sally"]
+
+def in_list?(name)
+  
+
+end
+
+
+user_name = gets.chomp
